87acebae2333bfb46ab5ff519b6b79ae2c92253b
Boris Bochkaryov <Boris-Bochkaryov@yandex.ru>
2015-10-20 20:06:49 +0600

    first test

---
+++ signal/testforgithub.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void term_handler(int i){  // обработчик сигнала
+	printf ("Пришёл сигнал %d\n",i);
+}
+int main() {
+	signal(SIGINT, term_handler); // переопределение
+	signal(SIGTERM, SIG_IGN); // игнорирование
+	raise(SIGINT); // отправка сигнала самому себе
+	printf("Мой pid %i\n", getpid()); 
+	printf("Ожидаем...\n"); 
+	while(1) sleep(1); 	
+	return 0;
+}
